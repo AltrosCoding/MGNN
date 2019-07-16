0165cc8abf02e6da52dc5f337f66b303874b7fa5
Altros <altros253@gmail.com>
2019-07-15 18:53:08 -0700

    First Commit

---
+++ database/migrations/2019_07_15_212722_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('user_name', 100)->unique();
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->datetime('birth_date');
+            $table->string('email', 100)->unique();
+            $table->boolean('is_confirmed')->default(false);
+            $table->string('role', 100);
+            $table->integer('exp')->default(0);
+            $table->integer('level')->default(1);
+            $table->string('ad_sense_snippet', 1000)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2019_07_16_011735_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 100);
+            $table->text('excerpt');
+            $table->text('content');
+            $table->string('featured_image', 1000);
+            $table->string('category', 100);
+            $table->string('tag', 1000);
+            $table->string('status', 100);
+            $table->datetime('created_at')
+                  ->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('updated_at')
+                  ->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->datetime('scheduled_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
