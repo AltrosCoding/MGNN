e96634241c5a3da7d574f8639be259a160566c1b
Altros <altros253@gmail.com>
2019-07-25 10:46:09 -0700

    Removed call to class Hash

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
     {
         $user = User::create([
             'user_name' => $request->user_name,
-            'password' => Hash::make($request->password),
+            'password' => $request->password,
             'first_name' => $request->first_name,
             'last_name' => $request->last_name,
             'birth_date' => $request->birth_date,
