46bcb23b9f90d46ffaf809ec96b908d33b8edd4c
Altros <altros253@gmail.com>
2019-07-29 16:48:39 -0700

    Implemented migration to remove old roles column

--- database/migrations/2019_07_29_232453_remove_role_column.php
+++ database/migrations/2019_07_29_232453_remove_role_column.php
@@ -14,6 +14,31 @@ class RemoveRoleColumn extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            $roles = \App\Role::all();
+
+            \App\User::all()->each(function ($user) use ($roles) {
+                switch ($user->role) {
+                    case 'admin':
+                        $user->roles()->attach($roles->where('name', 'Admin')->first()->id);
+                        break;
+                    
+                    case 'user':
+                        $user->roles()->attach($roles->where('name', 'User')->first()->id);
+                        break;
+                    
+                    case 'writer':
+                        $user->roles()->attach($roles->where('name', 'Contributor')->first()->id);
+                        break;
+                    
+                    case 'editor':
+                        $user->roles()->attach($roles->where('name', 'Jr. Editor')->first()->id);
+                        break;
+                    
+                    case 'seniorEditor':
+                        $user->roles()->attach($roles->where('name', 'Sr. Editor')->first()->id);
+                        break;
+                }
+            });
             $table->dropColumn('role');
         });
     }
