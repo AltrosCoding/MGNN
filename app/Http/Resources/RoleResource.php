6e0aef81037dc02db1c4c9a9291ec5baa52a35f1
Altros <altros253@gmail.com>
2019-07-29 11:32:18 -0700

    Foundation for role api

---
+++ app/Http/Resources/RoleResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RoleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $request->name,
+            'createComment' => $request->create_comment,
+            'createArticle' => $request->create_article,
+            'createRole' => $request->create_role,
+            'createUser' => $request->create_user,
+            'editComment' => $request->edit_comment,
+            'editArticle' => $request->edit_article,
+            'editRole' => $request->edit_role,
+            'editUser' => $request->edit_user,
+            'deleteComment' => $request->delete_comment,
+            'deleteArticle' => $request->delete_article,
+            'deleteRole' => $request->delete_role,
+            'deleteUser' => $request->delete_user,
+            'inviteAuthor' => $request->invite_author,
+            'revokeAuthor' => $request->revoke_author,
+        ];
+    }
+}
--- app/Role.php
+++ app/Role.php
@@ -6,8 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    protected $fillable = [
+        'name',
+        'create_comment',
+        'create_article',
+        'create_role',
+        'create_user',
+        'edit_comment',
+        'edit_article',
+        'edit_role',
+        'edit_user',
+        'delete_comment',
+        'delete_article',
+        'delete_role',
+        'delete_user',
+        'invite_author',
+        'revoke_author',
+    ];
+
     public function users()
     {
-        return $this->belongsToMany('App\User');
+        return $this
+                ->belongsToMany('App\User')
+                ->select([
+                    'id',
+                    'user_name',
+                    'first_name',
+                    'last_name',
+                    'role',
+                    'exp',
+                    'level',
+                    'ad_sense_snippet',
+                ]);
     }
 }
