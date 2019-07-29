3f3574e0ff1bcaf37fa477904900bb0ed29cb126
Altros <altros253@gmail.com>
2019-07-29 16:13:11 -0700

    Added database seeding for new role system

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,7 +13,29 @@ class UsersTableSeeder extends Seeder
     {
         factory(\App\User::class, 50)->create();
 
-        \App\User::where('role', '=', 'writer')
+        $roles = \App\Role::all()->pluck('id')->toArray();
+
+        \App\User::all()->each(function ($user) use ($roles) {
+            $history = array();
+            $toAdd = array();
+
+            for ($i = rand(1, 3); $i; --$i) {
+                $rand;
+
+                do {
+                    $rand = array_rand($roles);
+                } while (in_array($rand, $history));
+
+                array_push($history, $rand);
+                array_push($toAdd, $roles[$rand]);
+            }
+
+            $user->roles()->attach($toAdd);
+        });
+
+        \App\User::whereHas('roles', function (Illuminate\Database\Eloquent\Builder $query) {
+            $query->where('name', '=', 'Contributor');
+        })
         ->get()
         ->each(function ($user) {
             $posts = factory(\App\Post::class, rand(0, 3))->create();
